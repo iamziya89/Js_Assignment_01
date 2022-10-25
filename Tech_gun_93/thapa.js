51dfba7a1bac28c351c5fbc8f5b2810027d4f5cc
Mohd Ziya <iamziya53@gmail.com>
2022-10-25 10:09:12 +0530

    Practice

---
+++ Tech_gun_93/thapa.js
@@ -0,0 +1,8 @@
+var age=19;
+// if(age>=18){
+//     console.log("You are eligible");
+// }
+// else{
+//     console.log("You are not eligible");
+// }
+console.log((age>=18?"You are eligible":"you are not eligible"));
